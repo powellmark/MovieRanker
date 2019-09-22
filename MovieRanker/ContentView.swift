6d6e4fbb20a7915ca986e8d5a2f123e70b638e6c
Mark Powell <mark@aceable.com>
2019-09-22 13:17:39 -0500

    Initial Commit

---
+++ MovieRanker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieRanker
+//
+//  Created by Mark Powell on 9/22/19.
+//  Copyright © 2019 Lavacado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
